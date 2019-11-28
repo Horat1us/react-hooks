afa0e85af1260d399abed04704f35e5962970b4b
Alexander horat1us Letniko <reclamme@gmail.com>
2019-11-28 05:41:42 +0200

    Fix types for InputStateHook

--- src/use-input-state.ts
+++ src/use-input-state.ts
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export type InputStateHook<I extends HTMLInputElement = HTMLInputElement, S = React.HTMLProps<I>["value"]> =
+export type InputStateHook<I extends HTMLInputElement = HTMLInputElement, S extends React.HTMLProps<I>["value"] = string> =
     (initialState?: S) => [S, React.Dispatch<React.ChangeEvent<I>>, React.Dispatch<React.SetStateAction<S>>];
 export const useInputState: InputStateHook = (initialState = "") => {
     const [state, setState] = React.useState(initialState);
