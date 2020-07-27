2e813c5b8fd7111751fa2404355473cedc0d080f
horat1us <reclamme@gmail.com>
2020-07-27 11:54:52 +0300

    Add support for inputFilter in useInputState hook

--- src/use-input-state.ts
+++ src/use-input-state.ts
@@ -1,12 +1,14 @@
 import * as React from "react";
 
+export type InputFilter = (rawInput: string) => string;
 export type InputStateHook<I extends HTMLInputElement = HTMLInputElement, S extends React.HTMLProps<I>["value"] = string> =
-    (initialState?: S) => [S, React.Dispatch<React.ChangeEvent<I>>, React.Dispatch<React.SetStateAction<S>>];
-export const useInputState: InputStateHook = (initialState = "") => {
+    (initialState?: S, inputFilter?: InputFilter) => [S, React.Dispatch<React.ChangeEvent<I>>, React.Dispatch<React.SetStateAction<S>>];
+
+export const useInputState: InputStateHook = (initialState = "", inputFilter) => {
     const [state, setState] = React.useState(initialState);
     const handleChange = React.useCallback(
-        ({ target: { value } }) => setState(value),
-        [setState]
+        ({ target: { value } }) => setState(inputFilter ? inputFilter(value) : value),
+        [setState, inputFilter]
     );
     return [state, handleChange, setState];
 };
