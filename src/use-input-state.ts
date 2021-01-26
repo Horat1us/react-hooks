ae19bd69bb3134b645691586727e975aa01bac32
Eugene <grishchenko.ev86@gmail.com>
2021-01-26 17:59:38 +0200

    use-input-state additional types added

--- src/use-input-state.ts
+++ src/use-input-state.ts
@@ -1,7 +1,8 @@
 import * as React from "react";
 
 export type InputFilter = (rawInput: string) => string;
-export type InputStateHook<I extends HTMLInputElement = HTMLInputElement, S extends React.HTMLProps<I>["value"] = string> =
+export type InputType = HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement;
+export type InputStateHook<I extends InputType = InputType, S extends React.HTMLProps<I>["value"] = string> =
     (initialState?: S, inputFilter?: InputFilter) => [S, React.Dispatch<React.ChangeEvent<I>>, React.Dispatch<React.SetStateAction<S>>];
 
 export const useInputState: InputStateHook = (initialState = "", inputFilter) => {
